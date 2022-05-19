7b07a545f1a7ebd0139fb11e626c31bfffa94e4f
DevSchmidtchen <contact@schmidtchen.me>
2022-05-19 14:40:28 +0200

    feat: add measureComprehension boilerplate

--- package/lib/comprehension_measurement.dart
+++ package/lib/comprehension_measurement.dart
@@ -1,7 +1,28 @@
 library comprehension_measurement;
 
+import 'package:flutter/material.dart';
+
 /// A Calculator.
 class Calculator {
   /// Returns [value] plus 1.
   int addOne(int value) => value + 1;
+}
+
+void measureComprehension(BuildContext context) {
+  showBottomSheet(
+    context: context,
+    backgroundColor: Colors.transparent,
+    builder: (BuildContext context) {
+      return Container(
+        margin: const EdgeInsets.all(8),
+        width: double.infinity,
+        child: Card(
+          elevation: 4,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+          child: const Text("Text"),
+        ),
+      );
+    },
+  );
 }
