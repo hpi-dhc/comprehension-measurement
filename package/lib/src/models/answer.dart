4e291520b4f2adb57916ef455c85e771b1f48094
LeonHermann322 <Leon.Hermann@student.hpi.uni-potsdam.de>
2022-05-24 11:16:01 +0200

    added singleChoiceView

--- package/lib/src/models/answer.dart
+++ package/lib/src/models/answer.dart
@@ -4,9 +4,17 @@ part 'answer.g.dart';
 
 @JsonSerializable()
 class Answer {
-  Answer(this.id);
+  Answer(
+    this.id,
+    this.is_right,
+    this.answer_text,
+    this.times_selected,
+  );
 
-  int? id;
+  int id;
+  bool? is_right;
+  String answer_text;
+  int times_selected;
 
   factory Answer.fromJson(Map<String, dynamic> json) => _$AnswerFromJson(json);
   Map<String, dynamic> toJson() => _$AnswerToJson(this);
