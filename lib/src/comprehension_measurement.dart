b489c45a8a0656acfe791632cfd0d2ee3e48727a
DevSchmidtchen <contact@schmidtchen.me>
2022-05-25 13:56:24 +0200

    feat: wrap questions in page view

--- lib/src/comprehension_measurement.dart
+++ lib/src/comprehension_measurement.dart
@@ -1,4 +1,5 @@
 import 'package:comprehension_measurement/src/models/comprehension_measurement.dart';
+import 'package:comprehension_measurement/src/models/question.dart';
 import 'package:comprehension_measurement/src/types/single_choice.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -9,6 +10,7 @@ class ComprehensionMeasurementWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
+    final PageController controller = PageController();
     return Container(
       margin: const EdgeInsets.all(8),
       width: double.infinity,
@@ -19,28 +21,66 @@ class ComprehensionMeasurementWidget extends StatelessWidget {
           builder: (context, value, child) {
             return Padding(
               padding: const EdgeInsets.all(16.0),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Icon(
-                        Icons.emoji_emotions,
-                        color: theme.primaryColor,
-                      ),
-                      const SizedBox(
-                        width: 10,
-                      ),
-                      Text(value.survey!.questions[0].title),
-                    ],
-                  ),
-                  Divider(thickness: 2, color: theme.backgroundColor),
-                  SingleChoiceWidget(questionId: 1, model: value),
-                  ElevatedButton(
-                      onPressed: () => value.saveSingleChoiceAnswer(1),
-                      child: const Text('Send!'))
-                ],
+              child: PageView(
+                physics: const NeverScrollableScrollPhysics(),
+                controller: controller,
+                children: value.survey!.questions.map(
+                  (question) {
+                    Widget questionWidget;
+
+                    switch (question.type) {
+                      case QuestionType.single_choice:
+                        questionWidget = SingleChoiceWidget(
+                          questionId: question.id,
+                          model: value,
+                        );
+                        break;
+                      case QuestionType.multiple_choice:
+                        questionWidget = SingleChoiceWidget(
+                          questionId: question.id,
+                          model: value,
+                        );
+                        break;
+                      case QuestionType.text_answer:
+                        questionWidget = SingleChoiceWidget(
+                          questionId: question.id,
+                          model: value,
+                        );
+                        break;
+                    }
+
+                    return Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Icon(
+                              Icons.emoji_emotions,
+                              color: theme.primaryColor,
+                            ),
+                            const SizedBox(
+                              width: 10,
+                            ),
+                            Text(question.title),
+                          ],
+                        ),
+                        Divider(thickness: 2, color: theme.backgroundColor),
+                        questionWidget,
+                        ElevatedButton(
+                          onPressed: () {
+                            value.saveSingleChoiceAnswer(question.id);
+                            controller.nextPage(
+                              duration: const Duration(milliseconds: 400),
+                              curve: Curves.easeInOut,
+                            );
+                          },
+                          child: const Text('Send!'),
+                        )
+                      ],
+                    );
+                  },
+                ).toList(),
               ),
             );
           },
