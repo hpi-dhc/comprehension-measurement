8f1322030fbadd6a727eda8ea0665a5a367f0bf9
DevSchmidtchen <contact@schmidtchen.me>
2022-05-27 15:47:37 +0200

    feat: add completion widget

--- lib/src/comprehension_measurement.dart
+++ lib/src/comprehension_measurement.dart
@@ -1,5 +1,6 @@
 import 'package:comprehension_measurement/src/models/comprehension_measurement.dart';
 import 'package:comprehension_measurement/src/models/question.dart';
+import 'package:comprehension_measurement/src/types/completion.dart';
 import 'package:comprehension_measurement/src/types/multi_choice.dart';
 import 'package:comprehension_measurement/src/types/single_choice.dart';
 import 'package:comprehension_measurement/src/types/text_answer.dart';
@@ -28,14 +29,17 @@ class ComprehensionMeasurementWidget extends StatelessWidget {
                 Padding(
                   padding: const EdgeInsets.symmetric(
                     vertical: 8.0,
-                    horizontal: 16.0,
+                    horizontal: 0.0,
                   ),
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      Icon(
-                        Icons.emoji_emotions,
-                        color: theme.primaryColor,
+                      Padding(
+                        padding: const EdgeInsets.only(left: 16.0),
+                        child: Icon(
+                          Icons.assignment,
+                          color: theme.primaryColor,
+                        ),
                       ),
                       IconButton(
                         onPressed: () => Navigator.pop(context),
@@ -48,78 +52,94 @@ class ComprehensionMeasurementWidget extends StatelessWidget {
                   child: ExpandablePageView(
                     physics: const NeverScrollableScrollPhysics(),
                     controller: controller,
-                    children: value.survey!.questions.map(
-                      (question) {
-                        Widget questionWidget;
+                    children: [
+                      ...value.survey!.questions.map(
+                        (question) {
+                          Widget questionWidget;
 
-                        switch (question.type) {
-                          case QuestionType.single_choice:
-                            questionWidget = SingleChoiceWidget(
-                              questionId: question.id,
-                              model: value,
-                            );
-                            break;
-                          case QuestionType.multiple_choice:
-                            questionWidget = MultipleChoiceWidget(
-                              questionId: question.id,
-                              model: value,
-                            );
-                            break;
-                          case QuestionType.text_answer:
-                            questionWidget = TextAnswerWidget(
-                              questionId: question.id,
-                              model: value,
-                            );
-                            break;
-                        }
+                          switch (question.type) {
+                            case QuestionType.single_choice:
+                              questionWidget = SingleChoiceWidget(
+                                questionId: question.id,
+                                model: value,
+                              );
+                              break;
+                            case QuestionType.multiple_choice:
+                              questionWidget = MultipleChoiceWidget(
+                                questionId: question.id,
+                                model: value,
+                              );
+                              break;
+                            case QuestionType.text_answer:
+                              questionWidget = TextAnswerWidget(
+                                questionId: question.id,
+                                model: value,
+                              );
+                              break;
+                          }
 
-                        return Column(
-                          mainAxisSize: MainAxisSize.min,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Divider(
+                          return Column(
+                            mainAxisSize: MainAxisSize.min,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Divider(
+                                  thickness: 2,
+                                  height: 2,
+                                  color: theme.backgroundColor),
+                              Padding(
+                                padding: const EdgeInsets.symmetric(
+                                  vertical: 8.0,
+                                  horizontal: 16.0,
+                                ),
+                                child: Text(
+                                  question.title,
+                                ),
+                              ),
+                              Divider(
                                 thickness: 2,
                                 height: 2,
-                                color: theme.backgroundColor),
-                            Padding(
-                              padding: const EdgeInsets.symmetric(
-                                vertical: 8.0,
-                                horizontal: 16.0,
+                                color: theme.backgroundColor,
                               ),
-                              child: Text(
-                                question.title,
-                              ),
-                            ),
-                            Divider(thickness: 2, color: theme.backgroundColor),
-                            questionWidget,
-                            Padding(
-                              padding: const EdgeInsets.all(16.0),
-                              child: ElevatedButton(
-                                onPressed: () {
-                                  switch (question.type) {
-                                    case QuestionType.single_choice:
-                                      value.saveSingleChoiceAnswer(question.id);
-                                      break;
-                                    case QuestionType.multiple_choice:
-                                      value.saveMultipleChoiceAnswer(
-                                          question.id);
-                                      break;
-                                    case QuestionType.text_answer:
-                                      value.saveTextAnswer(question.id);
-                                      break;
-                                  }
-                                  controller.nextPage(
-                                    duration: const Duration(milliseconds: 400),
-                                    curve: Curves.easeInOut,
-                                  );
-                                },
-                                child: const Text('Send!'),
-                              ),
-                            )
-                          ],
-                        );
-                      },
-                    ).toList(),
+                              questionWidget,
+                              Align(
+                                alignment: Alignment.centerRight,
+                                child: Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                    vertical: 8.0,
+                                    horizontal: 16.0,
+                                  ),
+                                  child: ElevatedButton(
+                                    onPressed: () {
+                                      switch (question.type) {
+                                        case QuestionType.single_choice:
+                                          value.saveSingleChoiceAnswer(
+                                              question.id);
+                                          break;
+                                        case QuestionType.multiple_choice:
+                                          value.saveMultipleChoiceAnswer(
+                                              question.id);
+                                          break;
+                                        case QuestionType.text_answer:
+                                          value.saveTextAnswer(question.id);
+                                          break;
+                                      }
+
+                                      controller.nextPage(
+                                        duration:
+                                            const Duration(milliseconds: 400),
+                                        curve: Curves.easeInOut,
+                                      );
+                                    },
+                                    child: const Text('Send'),
+                                  ),
+                                ),
+                              )
+                            ],
+                          );
+                        },
+                      ).toList(),
+                      const CompletionWidget(),
+                    ],
                   ),
                 ),
               ],
