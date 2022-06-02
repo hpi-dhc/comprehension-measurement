1db25bcf81a374c0c372e9517354a56358c32f49
DevSchmidtchen <contact@schmidtchen.me>
2022-06-02 14:20:44 +0200

    feat: add db seed

---
+++ supabase/seed.sql
@@ -0,0 +1,25 @@
+insert into public.surveys (title)
+values 
+  ('Survey 1'),
+  ('Feedback Survey');
+
+insert into public.questions (title, type, is_contextual, context, survey_id)
+values
+  ('This is an example single choice question', 'single_choice', false, null, 1),
+  ('This is an example multiple choice question', 'multiple_choice', false, null, 1),
+  ('This is an example text question', 'text_answer', false, null, 1),
+  ('This is an example contextual single choice question', 'single_choice', true, 'context', 1),
+  ('This is an example contextual multiple choice question', 'multiple_choice', true, 'another_context', 1),
+  ('This is an open question for feedback', 'text_answer', false, null, 2);
+
+insert into public.answers (is_right, answer_text, question_id)
+values 
+  (true, 'This answer is true', 1),
+  (false, 'This answer is wrong', 1),
+  (true, 'This answer is true', 2),
+  (true, 'This answer is also true', 2),
+  (false, 'This answer is wrong', 2),
+  (null, 'This is a context answer', 4),
+  (null, 'This is another context answer', 4),
+  (null, 'This is a context answer', 5),
+  (null, 'This is another context answer', 5);
