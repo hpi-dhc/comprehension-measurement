6834c1fb437531575a58a45d3262b17a29d2b1a2
LeonHermann322 <Leon.Hermann@student.hpi.uni-potsdam.de>
2022-06-29 13:23:45 +0200

    added comprehension helper class

--- lib/comprehension_measurement.dart
+++ lib/comprehension_measurement.dart
@@ -1,3 +1,4 @@
 export 'src/main.dart' show measureComprehension;
 export 'src/comprehensible_page.dart' show AutoComprehensiblePage;
 export 'src/config.dart' show SupabaseConfig;
+export 'src/comprehension_helper.dart' show ComprehensionHelper;
---
+++ lib/src/comprehension_helper.dart
@@ -0,0 +1,52 @@
+import 'package:comprehension_measurement/comprehension_measurement.dart';
+import 'package:flutter/material.dart';
+
+class ComprehensionHelper {
+  static bool shouldMeasure = false;
+  static Map<String, List<int>> questionContext = {};
+
+  static void attach(
+    Future future, {
+    required BuildContext context,
+    required int surveyId,
+    required String introText,
+    required String surveyButtonText,
+    required SupabaseConfig supabaseConfig,
+    int? feedbackId,
+    String feedbackButtonText = 'Close',
+  }) {
+    ComprehensionHelper.shouldMeasure = true;
+    future.then((_) => ComprehensionHelper.measure(
+          context: context,
+          surveyId: surveyId,
+          introText: introText,
+          surveyButtonText: surveyButtonText,
+          supabaseConfig: supabaseConfig,
+          feedbackId: feedbackId,
+          feedbackButtonText: feedbackButtonText,
+        ));
+  }
+
+  static void measure({
+    required BuildContext context,
+    required int surveyId,
+    required String introText,
+    required String surveyButtonText,
+    required SupabaseConfig supabaseConfig,
+    int? feedbackId,
+    String feedbackButtonText = 'Close',
+  }) {
+    if (!shouldMeasure) return;
+    measureComprehension(
+      context: context,
+      surveyId: surveyId,
+      introText: introText,
+      surveyButtonText: surveyButtonText,
+      supabaseConfig: supabaseConfig,
+      questionContext: questionContext,
+      feedbackId: feedbackId,
+      feedbackButtonText: feedbackButtonText,
+    );
+    shouldMeasure = false;
+  }
+}
